6143324ddc27f4fffed7fa181467e9025fd244fa
Umer Mehmood <83476929+umer-r@users.noreply.github.com>
2021-08-23 11:46:13 -0700

    Bug Fix: Errors Suppressed in Output

--- sysmon.sh
+++ sysmon.sh
@@ -5,7 +5,7 @@ OpSt=$(hostnamectl status | grep -e Operat | sed -n -e 's/^.*System: //p')
 kerv=$(hostnamectl status | grep -e Ker | sed -n -e 's/^.*Kernel: //p')
 ipv4=$(hostname -I | cut -d " " -f 1)
 exip=$(curl -s -4 icanhazip.com)
-dns_=$(cat /etc/resolv.conf | grep 'name' | head -n 1 | awk {'print $2'})
+dns_=$(cat /etc/resolv.conf | grep 'name' | head -n 1 | awk {'print $2'}); if [[ $dns_ == "" ]]; then dns_="resolv.conf not Found!"; fi
 mcad=$(ip link show | grep link/ether | head -n 1 | awk {'print $2'})
 uptm=$(uptime -p)
 cpuc=$(lscpu | grep -e 'Architec\|^CPU(s):\|^Model name' | sed -n -e 's/^.*: //p' | tr -d '\n' | awk '{print $2", "$1", " substr($0,index($0,$3))}')
@@ -13,12 +13,8 @@ fmem=$(free -h | grep -i mem: | awk {'print $2" / "$4'})
 disk=$(df -h / | tail -n 1 | awk {'print $2"/"$3"->"$4"(Free)"'})
 load=$(uptime | sed -n -e 's/^.*average: //p')
 driv=$(parted --list | egrep "^Disk /dev" | awk '{print $2"-->("$3$4")  "}' | tr -d ':','\n')
-uuid=$(cat /etc/machine-id)
-frwl=$(systemctl status firewalld 2>/dev/null | grep Active | awk {'print $2"::"$3'} || systemctl status ufw 2>/dev/null | grep Active | awk {'print $2"::"$3'})
-if [[ $frwl == "" ]]
-then
-	frwl="Firewall not Found!"
-fi
+uuid=$(cat /etc/machine-id 2>/dev/null); if [[ $uuid == "" ]]; then uuid="machine-id not Found!"; fi
+frwl=$(systemctl status firewalld 2>/dev/null | grep Active | awk {'print $2"::"$3'} || systemctl status ufw 2>/dev/null | grep Active | awk {'print $2"::"$3'}); if [[ $frwl == "" ]]; then frwl="Firewall not Found!"; fi
 echo "+-----------------------+"
 echo "|Credits @UmerMehmood_  |"
 echo "+---------------+-------+"
